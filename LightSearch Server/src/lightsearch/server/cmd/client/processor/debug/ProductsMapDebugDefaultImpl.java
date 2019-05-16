ba7d6c0120ea412a3866658463a491642d12409a
ViiSE <viise@outlook.com>
2019-05-16 20:14:16 +1100

    Fixed products map debug typos

--- LightSearch Server/src/lightsearch/server/cmd/client/processor/debug/ProductsMapDebugDefaultImpl.java
+++ LightSearch Server/src/lightsearch/server/cmd/client/processor/debug/ProductsMapDebugDefaultImpl.java
@@ -27,11 +27,11 @@ public class ProductsMapDebugDefaultImpl implements ProductsMapDebug {
 
     private void initProducts() {
         ProductDebug pr1 = createProduct("Склад 1", "111111", "Товар 1", "100.0", "5", "шт.");
-        ProductDebug pr2 = createProduct("Склад 1", "222222", "Товар 2", "1500.0.", "10", "шт.");
+        ProductDebug pr2 = createProduct("Склад 1", "222222", "Товар 2", "1500.0", "10", "шт.");
         ProductDebug pr3 = createProduct("Склад 2", "111111", "Товар 1", "100.0", "100", "шт.");
         ProductDebug pr4 = createProduct("Склад 2", "444444", "Товар 4", "65.0", "7", "шт.");
         ProductDebug pr5 = createProduct("ТК 1", "111111", "Товар 1", "100.0", "7", "шт.");
-        ProductDebug pr6 = createProduct("ТК 2", "444444", "Товар 4", "650.0", "12", "шт.");
+        ProductDebug pr6 = createProduct("ТК 2", "444444", "Товар 4", "65.0", "12", "шт.");
         
         products.put("1", pr1);
         products.put("2", pr2);
