8a1097fd566c8b06d8dd4c2407931608e87cb698
ViiSE <viise@outlook.com>
2019-08-07 12:00:09 +1000

    Transition LightSearch Client Bot from Apache Ant to Apache Maven and fixed some typos

---
+++ LightSearch Client Bot/src/test/java/test/bot/processor/BotEntityProcessorAdvancedJSONTestNG.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2019 ViiSE.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package test.bot.processor;
+
+import lightsearch.client.bot.processor.BotEntityProcessor;
+import lightsearch.client.bot.processor.BotEntityProcessorAdvancedJSON;
+import lightsearch.client.bot.settings.BotSettingsReader;
+import lightsearch.client.bot.settings.BotSettingsReaderInit;
+import static org.testng.Assert.*;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static test.ResourcesFilesPath.getResourcesFilesPath;
+import static test.message.TestMessage.testBegin;
+import static test.message.TestMessage.testEnd;
+
+/**
+ *
+ * @author ViiSE
+ */
+public class BotEntityProcessorAdvancedJSONTestNG {
+    
+    private final String botSettingsFileName = getResourcesFilesPath() + "bot_settings_advanced_test.json";
+    private final int botAmount = 2;
+    private final String ip = "127.0.0.1";
+    private final int port = 50000;
+    private final long delayMessageDisplay = 5;
+    private Object data;
+    
+    @BeforeClass
+    public void setUpClass() {
+        BotSettingsReader botSettingsReader = BotSettingsReaderInit.botSettingsReader(botSettingsFileName);
+        data = botSettingsReader.data();
+    }
+
+    @Test
+    public void apply() {
+        testBegin("BotEntityProcessorAdvancedJSONTestNG", "apply()");
+        
+        assertNotNull(data, "Data is null!");
+        assertFalse(botAmount < 0, "Bot amount is less than 0!");
+        assertNotNull(ip, "IP is null!");
+        assertFalse(port < 1023 || port > 65535, "Wrong port!");
+        assertFalse(delayMessageDisplay < 0, "DelayMessageDisplay is null!");
+        
+        BotEntityProcessor botProc = new BotEntityProcessorAdvancedJSON(
+                        botAmount, ip, port, delayMessageDisplay);
+        assertNotNull(botProc, "BotEntityProcessor is null!");
+        
+        botProc.apply(data);
+        
+        testEnd("BotEntityProcessorAdvancedJSONTestNG", "apply()");
+    }
+}
