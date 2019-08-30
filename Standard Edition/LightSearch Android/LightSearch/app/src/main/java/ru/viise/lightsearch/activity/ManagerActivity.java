058a55b12154b363654f7887444259eed930e4c7
ViiSE <viise@outlook.com>
2019-08-30 16:52:07 +1000

    Imports optimized

--- Standard Edition/LightSearch Android/LightSearch/app/src/main/java/ru/viise/lightsearch/activity/ManagerActivity.java
+++ Standard Edition/LightSearch Android/LightSearch/app/src/main/java/ru/viise/lightsearch/activity/ManagerActivity.java
@@ -30,7 +30,6 @@ import android.telephony.TelephonyManager;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 import com.google.zxing.integration.android.IntentIntegrator;
